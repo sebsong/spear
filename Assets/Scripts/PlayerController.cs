f21f4c51b8b50b831f42238b5b66a0dba13a27a2
Sebastian Song <sebastianbsong@gmail.com>
2023-06-27 17:07:13 -0700

    basic movement and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float GravityForce;
+    public float Speed;
+    public float JumpSpeed;
+    public float JumpDuration;
+    
 
     private bool isAirborne;
+    private float jumpTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         isAirborne = true;
+        jumpTimer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Apply gravity
-        if (isAirborne) {
-            transform.position += Vector3.down * GravityForce * Time.deltaTime;
+        float horizontal_translation = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+
+        if (Input.GetButtonDown("Jump")) {
+            jumpTimer = JumpDuration;
         }
+        
+        float vertical_translation = 0f;
+        if (jumpTimer > 0) {
+            vertical_translation = JumpSpeed * Time.deltaTime;
+            jumpTimer -= Time.deltaTime;
+        } else if (isAirborne) {
+            // Apply Gravity
+            // transform.position += Vector3.down * GravityForce * Time.deltaTime;
+            vertical_translation = -GravityForce * Time.deltaTime;
+        }
+
+
+        transform.Translate(horizontal_translation, vertical_translation, 0);
+
         Debug.Log(isAirborne);
         
     }
